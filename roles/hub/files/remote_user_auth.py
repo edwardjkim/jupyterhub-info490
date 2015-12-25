e5fc6a12bb6684b842654226137914f0482d4a3d
Edward J Kim <edward.junhyung.kim@gmail.com>
2015-12-24 18:24:26 -0600

    linked shibboleth to remote user authentication

---
+++ roles/hub/files/remote_user_auth.py
@@ -0,0 +1,40 @@
+# https://github.com/cwaldbieser
+import os
+from jupyterhub.handlers import BaseHandler
+from jupyterhub.auth import Authenticator
+from jupyterhub.utils import url_path_join
+from tornado import gen, web
+from traitlets import Unicode
+
+
+class RemoteUserLoginHandler(BaseHandler):
+
+    def get(self):
+        header_name = self.authenticator.header_name
+        remote_user = self.request.headers.get(header_name, "")
+        if remote_user == "":
+            raise web.HTTPError(401)
+        else:
+            user = self.user_from_username(remote_user)
+            self.set_login_cookie(user)
+            self.redirect(url_path_join(self.hub.server.base_url, 'home'))
+
+
+class RemoteUserAuthenticator(Authenticator):
+    """
+    Accept the authenticated user name from the REMOTE_USER HTTP header.
+    """
+    header_name = Unicode(
+        default_value='Remote-User',
+        config=True,
+        help="""HTTP header to inspect for the authenticated username.""")
+
+    def get_handlers(self, app):
+        return [
+            (r'/login', RemoteUserLoginHandler),
+        ]
+
+    @gen.coroutine
+    def authenticate(self, *args):
+        raise NotImplementedError()
+
